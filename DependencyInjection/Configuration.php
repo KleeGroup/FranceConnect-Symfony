ac74685272fe8edaccc1fc3cc82edcaa63bdbc0e
Daif <daif.abderrahman@gmail.com>
2024-12-27 16:25:36 +0100

    Update Configuration.php change  poxy_port info (#5)

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -38,7 +38,7 @@ class Configuration implements ConfigurationInterface
                 ->defaultValue('')
             ->end()
             ->integerNode('proxy_port')
-                ->info('Proxy host.')
+                ->info('Proxy port.')
                 ->defaultValue(1080)
                 ->treatNullLike(1080)
                 ->defaultValue(null)
